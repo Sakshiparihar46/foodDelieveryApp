bd009fffc0fcdebb0060c30235e33fa2224a607a
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-13 14:46:49 +0530

    add some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Navbar from './components/navbar/navbar.jsx'
 function App() {
   return (
-    <div>
-      
+    <div className='app'>
+      <Navbar/>
     </div>
   )
 }
 
-export default App
+export default App;
