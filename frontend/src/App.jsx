a4ea5cc29da441b7e04ce17e83af948bc72ce0d8
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-21 20:08:44 +0530

    same new features added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from './components/navbar/navbar.jsx'
 import { Route,Routes } from 'react-router-dom';
 import Home from './pages/home/home.jsx'
@@ -6,11 +6,15 @@ import Cart from './pages/cart/cart.jsx'
 import PlaceOrder from './pages/placeOrder/placeOrder.jsx';
 import Footer from './components/Footer/footer.jsx';
 import AppDownload from './components/AppDownload/AppDownload.jsx';
+import LoginPopup from './components/LoginPopup/Loginpopup.jsx';
 function App() {
+
+  const [showLogin,setShowLogin]=useState(false);
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/cart' element={<Cart/>}/>
