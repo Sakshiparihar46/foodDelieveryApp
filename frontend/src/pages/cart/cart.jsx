5226ee55a8c1886a17ba1dcb0189e6b36f8b756d
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-23 20:41:51 +0530

    add add to cart function

--- frontend/src/pages/cart/cart.jsx
+++ frontend/src/pages/cart/cart.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import './cart.css'
+import { food_list } from "../../assets/assets";
+import { useContext } from 'react';
+import {StoreContext} from '../../context/context';
 const Cart=()=>{
-    return(
-        <div>
 
+    const {cartItems,food_list,removaFromCart}=useContext(StoreContext);
+    return(
+         <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+            <p>Items</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
         </div>
+        <br />
+        <hr />
+        {food_list.map((item,indx)=>{
+            if(cartItems[item._id]>0){
+                return(
+                <div className="cart-items-title card-items-item">
+                    <img src={item.image} alt="" />
+                    <p>{item.name}</p>
+                    <p>{item.price}</p>
+                    <p>{cartItems[item._id]}</p>
+                    <p>{item.price*cartItems[item._id]}</p>
+                    <p>X</p>
+                </div>
+                )
+            }
+        })}
+      </div>
+    </div>
     )
 }
 export default Cart;
