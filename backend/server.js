f8a6ccb4cc23b1db7efa9852a139cc7900787eac
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-01 21:48:48 +0530

    add remove property

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import foodRouter from './Routes/foodRoute.js';
 const app=express();
 let port=3000;
 
+// body parsers for POST data
+app.use(express.json());               // parse application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
@@ -15,6 +17,7 @@ connectDB();
 
 //api endpoints
 app.use("/api/food",foodRouter);
+app.use("/images", express.static('uploads'));  // serve uploaded files
 
 app.get("/",(req,res)=>{
     res.send("hello user");
