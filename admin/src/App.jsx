9da42a13622baea41edb3a812cae14fc03264044
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-06 01:38:39 +0530

    add routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
 import Navbar from './components/navbar/navbar'
 import SideBar from './components/sideBar/sideBar'
+import {Routes,Route} from 'react-router-dom'
+import Add from './pages/add/add.jsx'
+import List from './pages/list/list.jsx'
+import Orders from './pages/orders/orders.jsx'
 function App() {
   
 
@@ -11,6 +14,11 @@ function App() {
       <hr />
       <div className='app-content'>
         <SideBar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
     </>
