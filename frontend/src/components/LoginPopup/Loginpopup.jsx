4fdbab30de9281a2d0c364a78e21e3d5631daf29
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-21 23:31:38 +0530

    add frontend logic in login signup

--- frontend/src/components/LoginPopup/Loginpopup.jsx
+++ frontend/src/components/LoginPopup/Loginpopup.jsx
@@ -1,25 +1,51 @@
 import React,{useState} from 'react'
 import './LoginPopup.css'
 import {assets} from '../../assets/assets'
+import { StoreContext } from '../../context/context';
+
+
 function LoginPopup({setShowLogin}) {
+  const {url}=useConstext(StoreContext);
   const [currState,setCurrState]=useState("Login");
+  const [data,setData]=useState({
+    name:"",
+    email:"",
+    password:""
+  });
+
+  const onChangeHandler=(event)=>{
+    const name=event.target.name;
+    const value=event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  const onLogin=async (event)=>{
+    event.preventDefault();
+    
+  }
+
+
   return (
     <div className='login-popup'>
-      <form action="" className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className='login-popup-title'>
           <h2>{currState}</h2>
           <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
+
         <div className='login-popup-input'>
-          {currState==="Login"?<></>:<input type="text" placeholder='your name' required/>}
-          <input type="email" placeholder='your email' required/>
-          <input type="password" placeholder='password'  required/>
+          {currState==="Login"?<></>:<input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='your name' required/>}
+          <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='your email' required/>
+          <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='password'  required/>
         </div>
-        <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+
+        <button type='submit'>{currState==="Sign Up"?"Create account":"Login"}</button>
+
         <div className="login-popup-condition">
           <input type="checkbox" required/>
           <p>By continuing, i agree to the terms of use & privacy policy. </p>
         </div>
+
         {currState==="Login"? <p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>: <p>Already add an account? <span onClick={()=>setCurrState("Login")}>login here</span></p>}
       </form>
     </div>
