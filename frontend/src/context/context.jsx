99e896e44e8d707a3499d27d62dda3dd576e70fa
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-24 22:52:15 +0530

    add images and food item from the database

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,11 +1,12 @@
-import { createContext, useState } from "react"
-import { food_list } from "../assets/assets";
+import { createContext, useEffect, useState } from "react"
+import axios from "axios"
 export const StoreContext=createContext(null);
 
 const StoreContextProvoider=(props)=>{
     const [cartItems,setCartItems]=useState({});
     const url="http://localhost:3000"
     const [token,setToken]=useState("");
+    const [food_list,setFoodList]=useState([])
     const addToCart=(itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1 }))
@@ -27,6 +28,20 @@ const StoreContextProvoider=(props)=>{
         }
         return totalAmount;
    }
+
+    const fetchFoodList=async()=>{
+            const response = await axios.get(`${url}/api/food/list`);
+            setFoodList(response.data.data);
+    }
+   useEffect(()=>{
+    async function loadData(){
+        await fetchFoodList();
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"));
+        }
+    }
+    loadData();
+   },[]);
     
     const contextValue={
         food_list,
