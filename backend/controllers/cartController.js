90b1d42b1a2375dd6f11c7baeb22d225796b2a17
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-25 00:24:03 +0530

    add add to cart functinality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,20 @@ import userModel from "../models/userModel.js";
 //add items to user cart
 
 const addToCart=async (req,res)=>{
-
+    try{
+        let userData=await userModel.findOne({_id:req.body.userId});
+        let cartData=await userData.cartData;
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId]=1;
+        } else{
+            cartData[req.body.itemId]+=1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+        res.json({success:true,message:"added to cart"});
+    }catch(err){
+        console.log(error);
+        res.json({success:false,meassage:"error"});
+    }
 }
 
 //remove items from user cart
