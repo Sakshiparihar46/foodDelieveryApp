44c201baa110efbba1e3409ff94ede3a726118ba
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-26 22:07:55 +0530

    add ordercontroller or orderModel files in project

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from './Routes/foodRoute.js';
 import userRouter from './Routes/userRoute.js';
 import 'dotenv/config.js'
 import cartRouter from './Routes/cartRoute.js';
+import orderRouter from './Routes/orderRoute.js';
 
 
 const app=express();
@@ -23,6 +24,7 @@ app.use("/api/food",foodRouter);
 app.use("/images", express.static('uploads'));  
 app.use("/api/user",userRouter);
 app.use("/api/cart",cartRouter);
+app.use("/api/order",orderRouter);
 
 app.get("/",(req,res)=>{
     res.send("hello user");
