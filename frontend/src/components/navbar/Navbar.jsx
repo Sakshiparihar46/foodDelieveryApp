f8ac6173d224fbeebdd1573b91a3fe5626853870
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-31 01:56:22 +0530

    add myorders route

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar=({setShowLogin})=>{
             {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>:<div className="navbar-profile">
                 <img src={assets.profile_icon} alt="" />
                 <ul className="nav-profile-dropdown">
-                    <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                    <Link to='/myorders'><li><img src={assets.bag_icon} alt="" /><p>Orders</p></li></Link>
                     <hr />
                     <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
                 </ul>
