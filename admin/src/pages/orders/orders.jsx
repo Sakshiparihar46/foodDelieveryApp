09b1d7bb757efc9844d84e7494ee8fd69ddf5b00
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-31 01:47:01 +0530

    add updateStatus function

--- admin/src/pages/orders/orders.jsx
+++ admin/src/pages/orders/orders.jsx
@@ -16,6 +16,16 @@ function Orders({url}) {
         }
       }
 
+      const statusHandler=async(event,orderId)=>{
+       const response=await axios.post(url+"/api/order/status",{
+        orderId,
+        status:event.target.value
+       })
+       if(response.data.success){
+        await fetchAllOrders();
+       }
+      }
+
       useEffect(()=>{
         fetchAllOrders();
       },[])
@@ -51,7 +61,7 @@ function Orders({url}) {
             </div>
             <p>Items:{order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandler(event,order._id)}  value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for Delievery">Out for Delievery</option>
               <option value="Delivered">Delivered</option>
